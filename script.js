83036a0269accc372220018e4587fe4b61107067
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-22 09:35:25 +0600

    update 3 lines

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let btn = document.querySelector("#h2");
 btn.onclick =() =>{
  console.log("Aamre Kik Dise");
-//     let a = 25;
-// a++;
-// console.log(a);
+    let a = 25;
+a++;
+console.log(a);
 
 };

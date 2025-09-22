aa1e47689c25f62773a9778aca0d808de4394311
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-22 10:39:45 +0600

    increment

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
 let btn = document.querySelector("#h2");
-btn.onclick =() => {
- console.log("Aamre Kik Dise");
+btn.onclick = () => {
+    console.log("Aamre Kik Dise");
     let a = 25;
-a++;
-console.log(a);
+    a++;
+    console.log(a);
 };
 
 // ============ Even Object =============== 
 let btn1 = document.querySelector("#h2");
-btn1.onclick =(evt) => {
-//  console.log("Aamre Kik Dise");
-//     let a = 25;
-// a++;
-console.log(evt);
-console.log(evt.target);
+btn1.onclick = (evt) => {
+    //  console.log("Aamre Kik Dise");
+    //     let a = 25;
+    // a++;
+    console.log(evt);
+    console.log(evt.target);
 
-console.log(evt.type);
-console.log(evt.clientX, evt.clientY);
+    console.log(evt.type);
+    console.log(evt.clientX, evt.clientY);
 
 };
 
 
 // ============= use EventListener........ ============
 let btns = document.querySelectorAll("button");
+btns.forEach((clickBtn ) => {
+    clickBtn.addEventListener("click", () => {
 
-btns.forEach((clickBtn) => {
-clickBtn.addEventListener("click" , () => {
-    // clickBtn.innerText = "Amare Click Dise"
+      
+        console.log("Hello");
     
-    let newH2 = document.createElement("h2");
-    newH2.innerText = " Hello";
-    clickBtn.parentElement.append(newH2);
-console.log(newH2);
+    });
+});
 
+
+
+
+// ============== * increment Hello *  + create Hello           ============    
+
+let allBtns = document.querySelectorAll("button");
+let counter = 1;
+
+btns.forEach((clickBtn ) => {
+    clickBtn.addEventListener("click", () => {
+        // clickBtn.innerText = "Amare Click Dise"
+
+        let newH2 = document.createElement("h2");
+        newH2.innerText =  `Hello ${counter}`;
+        clickBtn.parentElement.append(newH2);
+        console.log(newH2);
+     counter++;
+    });
 });
-});
+
 
 
 // ============= use EventListener...to  Mouse Over..... ============
 let divs = document.querySelectorAll("divs")
 divs.forEach((hoverDivs) => {
-    hoverDivs.addEventListener("mouseover" , () => {
-console.log("Amare Teach korse  😠 ");
-console.log(hoverDivs.target);
-});
+    hoverDivs.addEventListener("mouseover", () => {
+        console.log("Amare Teach korse  😠 ");
+        console.log(hoverDivs.target);
+    });
 });
 
